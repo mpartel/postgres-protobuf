0e55e0a0286b6a1124f06057ed01fe5f1bdd7842
Martin Pärtel <martin.partel@gmail.com>
2020-12-01 09:42:26 +0200

    Fixed some obsolete protobuf library paths

--- build-protobuf-library.sh
+++ build-protobuf-library.sh
@@ -5,7 +5,7 @@ PROTOBUF_VERSION=3.11.2
 
 cd "$(dirname "${0}")"
 
-rm -Rf third_party/protobuf-*
+rm -Rf third_party/protobuf-* third_party/protobuf
 mkdir -p third_party/
 cd third_party
 curl -L "https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-cpp-${PROTOBUF_VERSION}.tar.gz" | tar xzf -
